f7ff1ac8e35a2fb85a1f4d62ddcb5a837ecc74ca
pgarces328-sudo <pgarces328@gmail.com>
2026-09-23 12:42:40 -0500

    chore: iniciar semana 08

--- week-07-persistencia_local/3-proyecto/starter/src/navigation/types.ts
+++
@@ -1,2 +0,0 @@
-export type RootTabParamList = { ShipmentsTab: undefined; TrackedTab: undefined; DriversTab: undefined; RoutesTab: undefined; SettingsTab: undefined; };
-export type ShipmentsStackParamList = { ShipmentsList: undefined; ShipmentDetail: { id: string }; CreateShipment: undefined; EditShipment: { id: string }; };
